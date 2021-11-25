9d806ca2c365a5106d11d60ac85acf1e16f6cc2e
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-11-25 19:17:38 +0100

    function handleClick

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -10,6 +10,9 @@ const Nav = () => {
     //par default elle ne sera pas noir
     const [navBlack, setNavBlack] = useState(false);
 
+    //afficher le menu burger
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     const transitionNav = () => {
         //on veut verifier que lorsqu'on scroll verticalelment, si on est au dessus de 100 zlors on met notre class black
         window.scrollY > 100 ? setNavBlack(true) : setNavBlack(false);
@@ -19,12 +22,21 @@ const Nav = () => {
         //quuand on va scroller on va appeler la function du dessus
         document.addEventListener("scroll", transitionNav);
     });
-    // Detection de la transition du scroll
+
+    // au click sur  mon button, si toggleMenu est à true alors je le passe à false, et inversement
+    const handleClick = () => {
+        toggleMenu ? setToggleMenu(false) : setToggleMenu(true);
+    };
 
     // Afficher le menu au click sur le burger
     return (
-        <div className={` nav ${navBlack && "nav__black"} `}>
-            <button className="nav__burger">
+        //si toggleMenu / navBlack sont à true alors je leur attribu leur class
+        <div
+            className={` nav ${navBlack && "nav__black"} ${
+                toggleMenu && "show"
+            }`}
+        >
+            <button className="nav__burger" onClick={handleClick}>
                 <MenuIcon />
             </button>
             <img src="./image/logo.png" className="nav__logo" alt="logo"></img>
