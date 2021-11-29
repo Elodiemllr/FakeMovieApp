94648a6adba10b8465860b1b81e393109c5f4cf4
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-11-29 19:24:56 +0100

    mise en place des catégories dans app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
 import Banner from "./components/Banner.js";
 import Footer from "./components/footer.js";
 import Nav from "./components/nav.js";
+import Row from "./components/Row.js";
+import Requests from "./config/data/Request.js";
 import "./styles/App.scss";
 
 function App() {
     return (
         <div className="App">
-            {/* nav bar  */}
             <Nav />
-            {/* banner  */}
             <Banner />
-            {/* cat film liste  */}
+            {/* catégorie film liste, 1 row = 1 cat  */}
+            <Row
+                title="Netflix Original Movies"
+                fetchUrl={Requests.fetchNetflixOriginals}
+            />
+
+            <Row title="Tendance Actuelles" fetchUrl={Requests.fetchTrending} />
+
+            <Row title="Top 10" fetchUrl={Requests.fetchTopRated} />
+
+            <Row
+                title="Comédie Romantique"
+                fetchUrl={Requests.fetchRomanceMovies}
+            />
 
             {/* video */}
 
---
+++ client/src/components/Row.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Row.scss";
+
+const Row = ({ title, fetchUrl }) => {
+    return <div>coucou</div>;
+};
+
+export default Row;
