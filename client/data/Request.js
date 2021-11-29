65accba0eb43e9773f826108917fc7b4afb9bd44
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-11-29 15:55:51 +0100

    adding request link

--- client/data/Request.js
+++ client/data/Request.js
@@ -1,5 +1,15 @@
-//fa6abf7462c6eb8d93de6267251a5b83
-//https://api.themoviedb.org/3
-
 const API_KEY = "fa6abf7462c6eb8d93de6267251a5b83";
 const BASE_URL = "https://api.themoviedb.org/3";
+
+const Requests = {
+    //ici tout les links trouvé dans la documentation
+    fetchTrending: `${BASE_URL}/trending/all/week?api_key=${API_KEY}`,
+    fetchNetflixOriginals: `${BASE_URL}/trending/all/week?api_key=${API_KEY}`,
+    fetchTopRated: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&width_genres=28`,
+    fetchHorrorMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&width_genres=35`,
+    fetchRomanceMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&width_genres=10749`,
+    fetchDocumentaries: `${BASE_URL}/discover/movie?api_key=${API_KEY}&width_genres=99`,
+};
+
+export default Requests;
