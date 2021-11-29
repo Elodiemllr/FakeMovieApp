fa0cfad95f5f1bd3f169373db16558e155827f7e
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-11-29 20:24:34 +0100

    ajout du css de base et des images de row

--- client/src/components/Row.js
+++ client/src/components/Row.js
@@ -1,8 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "../styles/Row.scss";
-
 const Row = ({ title, fetchUrl }) => {
-    return <div>coucou</div>;
+    //on recup tout les films
+    const [movies, setMovies] = useState([]);
+
+    //vu qu'on utilisé axios, besoin du useEffect
+    useEffect(() => {
+        async function fetchData() {
+            //on recup notre url  qu'on stock dans request
+            const request = await axios.get(fetchUrl);
+
+            //on va chercher un film, puis celui choisit sera stocké dans movie
+            setMovies(
+                //le chemin au niveau des data de moviedb , ou il y'aura tout nos results
+                request.data.results
+            );
+        }
+        //on active la function
+        fetchData();
+    }, []);
+
+    return (
+        <div className="row">
+            <h2 className="row__title">title </h2>
+            <div className="row__images">
+                {movies.maps((movie) => (
+                    <div key={movie.id}>
+                        <img
+                            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                            alt="movie image"
+                            className="row__image"
+                        />
+                    </div>
+                ))}
+            </div>{" "}
+        </div>
+    );
 };
 
 export default Row;
