6278c0c5d0d80c49a408fa30f3e80637cc0d296a
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2022-01-10 15:28:27 +0100

    useparams id on video.js

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { useParams } from "react-router";
 import "../styles/Video.scss";
 
 const Video = () => {
-    return <div className="video"></div>;
+    let { id } = useParams();
+    return (
+        <div className="video">
+            {" "}
+            <iframe
+                src={`https://www.youtube.com/embed/${id}`}
+                title="video"
+                frameBorder="0"
+                allowFullScreen
+            >
+                {" "}
+            </iframe>
+        </div>
+    );
 };
 
 export default Video;
